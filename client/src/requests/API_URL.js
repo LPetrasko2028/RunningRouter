21ab17ec8dffffbca872a4aa5d34c342f6227d0f
LPetrasko2028 <118296820+LPetrasko2028@users.noreply.github.com>
2024-04-22 13:27:35 -0500

    Update API_URL.js

--- client/src/requests/API_URL.js
+++ client/src/requests/API_URL.js
@@ -1,2 +1,5 @@
-export const API_URL = 'http://localhost:3000/api';
-export const FLASK_URL = 'http://localhost:5000';
+// export const API_URL = 'http://localhost:3000/api';
+// export const FLASK_URL = 'http://localhost:5000';
+
+export const API_URL = 'https://starfish-app-6mqw2.ondigitalocean.app/api';
+export const FLASK_URL = '';
